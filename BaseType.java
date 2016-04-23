44447ea072bdb83376280d1ff79bc8b576e63650
Charanya R <ravichandran.charanya@gmail.com>
2016-04-23 21:24:36 +0530

    Create BaseType.java

---
+++ BaseType.java
@@ -0,0 +1,24 @@
+/**
+*This class demonstrates java base types holding default values.
+*
+**/
+
+public class BaseType {
+  boolean booleanType;
+  int intType;
+  char charType;
+  short shortType;
+  float floatType;
+  double doubleType;
+  long longType;
+  
+  System.out.println(booleanType);
+  System.out.println(intType);
+  System.out.println(charType);
+  System.out.println(shortType);
+  System.out.println(floatType);
+  System.out.println(doubleType);
+  System.out.println(longType);
+
+
+}
